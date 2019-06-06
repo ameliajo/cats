e74ae43536d45a13299bf1fc8b2acc5edb0fa7a9
Amelia Trainer <ameliajo@mit.edu>
2019-06-06 12:08:06 -0600

    added in simple test

---
+++ src/cats.h
@@ -0,0 +1,5 @@
+#include <iostream>
+
+void cats(){
+  std::cout << "hello, world" << std::endl;
+}
---
+++ src/test/cats.test.cpp
@@ -0,0 +1,9 @@
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+
+
+TEST_CASE( "" ){
+  GIVEN( "" ){ 
+    REQUIRE( true );
+  } // GIVEN
+} // TEST CASE
