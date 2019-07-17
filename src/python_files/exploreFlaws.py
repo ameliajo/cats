eef4ee9bad396d4447d3c0de8ed04370a4c92b68
Amelia Trainer <ameliajo@mit.edu>
2019-07-17 11:26:50 -0400

    included flaw exploration - basically made lots of plots about how i wasn't capturing fine oscillations

---
+++ src/python_files/exploreFlaws.py
@@ -0,0 +1,59 @@
+from phononDists import *
+import numpy as np
+import matplotlib.pyplot as plt
+from math import pi
+#import matplotlib.colors as colors
+#import matplotlib.cm as cmx
+
+def getP(beta,betas,rho):
+    half = int(len(rho)*0.5+1)
+    return rho[half]/(betas[half]*betas[half]) if abs(beta) < betas[half] else \
+           np.interp(beta,betas,rho)/(2.0*beta*np.sinh(beta*0.5)) 
+
+
+original_rho   = rho[::-1] + rho[1:]
+original_betas = [-x for x in betas][::-1] + betas[1:]
+original_P = [getP(beta,original_betas,original_rho) for beta in original_betas]
+
+thin_betas = list(np.linspace(original_betas[0],original_betas[-1],1000))
+thin_P = [getP(beta,original_betas,original_rho) for beta in thin_betas]
+
+#plt.plot(original_betas,original_P,linewidth=2)
+#plt.plot(thin_betas,thin_P,linewidth=1)
+#plt.title('F integrand = P(b)*exp(-b/2)*cos(b*t), t = 62.85 = 2pi/deltaBeta')
+#plt.xlabel('beta')
+
+original_integrals = []
+thin_integrals = []
+
+t = 62.84985568390593
+t_vec = list(np.linspace(0.0,2*t+2,5000))
+for i,t in enumerate(t_vec):
+    if (i%500==0):
+        print(100*i/len(t_vec))
+    original_f = [original_P[i]*np.exp(-original_betas[i]*0.5)*np.cos(-original_betas[i]*t) for i in range(len(original_betas))]
+    thin_f = [thin_P[i]*np.exp(-thin_betas[i]*0.5)*np.cos(-thin_betas[i]*t) for i in range(len(thin_betas))]
+
+    #plt.plot(original_betas,original_f,label='original beta grid')
+    #plt.plot(thin_betas,thin_f,label='thin beta grid')
+    original_integrals.append(np.trapz(original_f,x=original_betas))
+    thin_integrals.append(np.trapz(thin_f,x=thin_betas))
+
+
+
+
+
+
+plt.title('F(t)')
+plt.xlabel('t')
+plt.plot(t_vec,original_integrals,label='original grid')
+plt.plot(t_vec,thin_integrals,label='thin grid')
+plt.legend(loc='best')
+plt.show()
+
+
+
+
+
+
+
--- src/python_files/trapzSAB.py
+++ src/python_files/trapzSAB.py
@@ -31,36 +31,69 @@ def finishPlotting(colorBar,title):
 
 
 
+
 P = [rho[1]/(betas[1]*betas[1])] + \
     [rho[i]/(2.0*betas[i]*np.sinh(betas[i]*0.5)) for i in range(1,len(rho))]
 fullP = P[::-1]+P[1:]
-
 fullBetas = [-x for x in betas][::-1] + betas[1:]
+fullRho = rho[::-1] + rho[1:]
+
+"""
+plt.plot(fullBetas,fullP)
+
+def getP(rhoVal,betaVal):
+    if abs(betaVal)<betas[1]:
+        return rho[1]/(betas[1]*betas[1])
+    return rhoVal/(2.0*betaVal*np.sinh(betaVal*0.5)) 
+
+thinBetas = list(np.linspace(fullBetas[0],fullBetas[-1],1e3))
+thinRho   = [np.interp(x, fullBetas, fullRho) for x in thinBetas]
+thinP     = [getP(thinRho[i],thinBetas[i]) for i in range(len(thinBetas))]
+
+plt.plot(fullBetas,fullRho)
+plt.plot(thinBetas,thinRho)
+plt.plot(thinBetas,thinP)
+plt.show()
+exit()
+"""
+
+
+
 
 def getF(t,fullBetas,fullP):
     F_integrand = [fullP[i]*np.exp(-fullBetas[i]*0.5)*np.cos(fullBetas[i]*t) \
                    for i in range(len(fullP))]
     return np.trapz(F_integrand,x=fullBetas)
 
+def getP(rhoVal,betaVal):
+    if abs(betaVal)<betas[1]:
+        return rho[1]/(betas[1]*betas[1])
+    return rhoVal/(2.0*betaVal*np.sinh(betaVal*0.5)) 
+thinBetas = list(np.linspace(fullBetas[0],fullBetas[-1],5e2))
+thinRho   = [np.interp(x, fullBetas, fullRho) for x in thinBetas]
+thinP     = [getP(thinRho[i],thinBetas[i]) for i in range(len(thinBetas))]
+
+def getF_better(t,fullBetas,fullP):
+    F_integrand = [thinP[i]*np.exp(-thinBetas[i]*0.5)*np.cos(thinBetas[i]*t) \
+                   for i in range(len(thinP))]
+    return np.trapz(F_integrand,x=thinBetas)
+ 
+
 def getG(t,fullBetas,fullP):
     G_integrand = [fullP[i]*np.exp(-fullBetas[i]*0.5)*np.sin(fullBetas[i]*t) \
                    for i in range(len(fullP))]
     return np.trapz(G_integrand,x=fullBetas)
 
-#plt.plot(fullBetas,rho[::-1]+rho[1:],label='rho(b)')
-#plt.plot(fullBetas,fullP,label='P(b)')
-#plt.plot(fullBetas,[fullP[i]*np.exp(-fullBetas[i]*0.5) for i in range(len(fullBetas))],label='P(b)*exp(-b/2)')
-#times = [0.0,1.0,2.0,3.0,10.0,30.0,60.0,61.0,62.85]
+"""
 times = [0.0,1.0,2.0,3.0,10.0]
 scalarMap,colorBar = prepPlot(times)
 for i,t in enumerate(times):
     plt.plot(fullBetas,[np.cos(fullBetas[i]*t)*fullP[i]*np.exp(-fullBetas[i]*0.5) for i in range(len(fullBetas))],color=scalarMap.to_rgba(i))
     plt.fill(fullBetas,[np.cos(fullBetas[i]*t)*fullP[i]*np.exp(-fullBetas[i]*0.5) for i in range(len(fullBetas))],color=scalarMap.to_rgba(i),alpha=0.2)
-
 plt.xlabel('beta')
 finishPlotting(colorBar,'')
-
 exit()
+"""
 
 alpha = 1.01
 beta = 0.006
@@ -68,27 +101,34 @@ beta = 0.00255*3/(kb*296)
 print(beta,2*pi/beta)
 
 F_contrib, G_contrib, sabContrib = [], [], []
+F_better_contrib = []
 partial1, partial2, partial3, integral = [], [], [], []
 #t_vec = np.linspace(0,50000,10000)
 #t_vec = np.linspace(0,8*2*pi/betas[1],1e4)
 #t_vec = np.linspace(0,40*2*pi/betas[1],1e5)
-t_vec = np.linspace(0,20*2*pi/betas[1],1e5)
+t_vec = np.linspace(0,14*2*pi/betas[1],2e4)
 for i,t in enumerate(t_vec):
        
     F = getF(t,fullBetas,fullP)
-    G = getG(t,fullBetas,fullP)
+    F_better = getF_better(t,fullBetas,fullP)
+    #G = getG(t,fullBetas,fullP)
     F_contrib.append(F)
-    G_contrib.append(G)
-    sabContrib.append( np.exp(alpha*F) * np.cos(beta*t - alpha*G) )
-    partial1.append( np.cos(beta*t - alpha*G) )
-    partial2.append( np.exp(alpha*F) )
-    integral.append(np.trapz(sabContrib,x=t_vec[:i+1]))
-    #partial3.append( np.exp(alpha*F) * np.cos(beta*t) )
+    F_better_contrib.append(F_better)
+    partial1.append(np.trapz(F_contrib,x=t_vec[:i+1]))
+    partial2.append(np.trapz(F_better_contrib,x=t_vec[:i+1]))
+    #G_contrib.append(G)
+    #sabContrib.append( np.exp(alpha*F) * np.cos(beta*t - alpha*G) )
+    #partial1.append( np.cos(beta*t - alpha*G) )
+    #partial2.append( np.exp(alpha*F) )
+    #integral.append(np.trapz(sabContrib,x=t_vec[:i+1]))
     if (int(i)%1000==0):
         print((i*100/len(t_vec)))
  
 
 plt.plot(t_vec,F_contrib,label='F')
+plt.plot(t_vec,F_better_contrib,label='F (better)')
+plt.plot(t_vec,partial1,label='F integral')
+plt.plot(t_vec,partial2,label='F (better) integral')
 #plt.plot(t_vec,G_contrib,label='G')
 #plt.plot(t_vec,partial2,label='e^(a*F)')
 #plt.plot(t_vec,partial1,label='cos(b*t-a*G)')
