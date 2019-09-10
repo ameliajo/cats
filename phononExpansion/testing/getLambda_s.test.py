1ce710fa66f4132da77d20a8c384a7d2dbb3c677
Amelia Trainer <ameliajo@ugps-MacBook.local>
2019-09-10 09:59:43 -0500

    wrote njoy python code

--- phononExpansion/testing/getLambda_s.test.py
+++ phononExpansion/testing/getLambda_s.test.py
@@ -3,15 +3,16 @@ sys.path.append("..")
 from phononExpansion import *
 import unittest
 import pytest
+from dos import *
 
 class TestLambda_s(unittest.TestCase):
 
     def test_lambda_s(self):
-        delta = 0.03
-        rho = [-(i-20)**2+400 for i in range(40)] + [10 for i in range(40,47)] + [-(i-70)**2+500 for i in range(48,93)] + [0.0]
+        delta = delta_parabolas[:]
+        rho = rho_parabolas[:]
         betaGrid = [delta*i for i in range(len(rho))]
-        lambda_s = getLambda_s(rho,betaGrid)
-        print(lambda_s)
+        #lambda_s = getLambda_s(rho,betaGrid)
+        #print(lambda_s)
 
         assert True == True
 
