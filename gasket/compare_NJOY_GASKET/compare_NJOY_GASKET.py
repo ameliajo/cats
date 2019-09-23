bd1dab5af08be9a0c089bde74f1679e6e18b27a2
Amelia Trainer <ameliajo@mit.edu>
2019-09-23 18:34:55 -0400

    adding more comparison files between the two

--- gasket/compare_NJOY_GASKET/compare_NJOY_GASKET.py
+++ gasket/compare_NJOY_GASKET/compare_NJOY_GASKET.py
@@ -6,24 +6,25 @@ import sys
 sys.path.append("NJOY/")
 sys.path.append("GASKET/")
 
-from njoy_output_1 import *
-from gasket_output_1 import *
+#from njoy_output_1 import *
+#from gasket_output_1 import *
+from njoy_output_2 import *
+from gasket_output_2 import *
+#from njoy_output_3 import *
+#from gasket_output_3 import *
+
+
+
+
 
 def prepPlot(vector):
     cnorm = colors.Normalize(vmin=0,vmax=2*len(vector)+1)
-    #scalarMap = cmx.ScalarMappable(norm=cnorm,cmap=plt.get_cmap('hot')) #hot autumn tab10
-    #scalarMap = cmx.ScalarMappable(norm=cnorm,cmap=plt.get_cmap('autumn')) #hot autumn tab10
     scalarMap = cmx.ScalarMappable(norm=cnorm,cmap=plt.get_cmap('tab20')) #hot autumn tab10
     mymap = colors.LinearSegmentedColormap.from_list('funTestColors',\
             [scalarMap.to_rgba(a) for a in range(len(vector))])
     colorBar = plt.contourf([[0,0],[0,0]], vector, cmap=mymap)
     plt.clf()
     return scalarMap, colorBar
-
-
-
-
-
 
 
 assert(len(alphas) == len(gasket_alphas))
@@ -33,26 +34,26 @@ for b in range(len(betas)):  assert(abs(betas[b]-gasket_betas[b])<1e-5)
 
 
 
-fig = plt.figure(num=None, figsize=(17, 12), dpi=80, facecolor='w', edgecolor='k')
-#fig = plt.figure()
+#fig = plt.figure(num=None, figsize=(17, 12), dpi=80, facecolor='w', edgecolor='k')
+fig = plt.figure()
 scalarMap, colorBar = prepPlot(alphas)
 
 
 for a in range(0,len(alphas)):
     njoy_sym = [sab_nonsym_negative_side[b+a*len(betas)] * \
                       np.exp(-betas[b]*0.5) for b in range(len(betas))]
-    gasket_sym = [0.0255*gasket_sab[b+a*len(betas)] for b in range(len(betas))]
-    plt.plot(betas,njoy_sym,color=scalarMap.to_rgba(a),linewidth=2)
-    if gasket_sym[-1] < 1e-7:
-        plt.plot(betas[:-1],gasket_sym[:-1],color=scalarMap.to_rgba(a),marker='o',markersize=3,linewidth=0)
-    else:
-        plt.plot(betas,gasket_sym,color=scalarMap.to_rgba(a),marker='o',markersize=3,linewidth=0)
+    gasket_sym = [0.02550730568*gasket_sab[b+a*len(betas)] for b in range(len(betas))]
+    difference = [gasket_sym[i] - njoy_sym[i] for i in range(len(njoy_sym))] 
+    plt.plot(betas,njoy_sym,color=scalarMap.to_rgba(a),linewidth=3)
+    plt.plot(betas[:-1],gasket_sym[:-1],color=scalarMap.to_rgba(a),\
+             marker='o',markersize=3,linewidth=2,linestyle='dashed')
+    #plt.plot(betas[:-1],difference[:-1],color=scalarMap.to_rgba(a))
 
 
 
 ax = plt.gca()
 plt.colorbar(colorBar).ax.set_ylabel('alpha values')
-plt.yscale('log')
+#plt.yscale('log')
 plt.show()
 #plt.draw()
 #plt.pause(1) 
