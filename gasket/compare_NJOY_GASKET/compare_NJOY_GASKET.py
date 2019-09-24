532691f25feeec0b3c6d13afa45d8c65b9e82cb6
Amelia Trainer <ameliajo@mit.edu>
2019-09-24 09:43:18 -0400

    redid some of the plotting to make it flow easier

--- gasket/compare_NJOY_GASKET/compare_NJOY_GASKET.py
+++ gasket/compare_NJOY_GASKET/compare_NJOY_GASKET.py
@@ -6,13 +6,6 @@ import sys
 sys.path.append("NJOY/")
 sys.path.append("GASKET/")
 
-#from njoy_output_1 import *
-#from gasket_output_1 import *
-from njoy_output_2 import *
-from gasket_output_2 import *
-#from njoy_output_3 import *
-#from gasket_output_3 import *
-
 
 
 
@@ -26,38 +19,71 @@ def prepPlot(vector):
     plt.clf()
     return scalarMap, colorBar
 
+def endPlot(colorBar):
+    ax = plt.gca()
+    plt.colorbar(colorBar).ax.set_ylabel('alpha values')
+    plt.yscale('log')
+    plt.show()
 
-assert(len(alphas) == len(gasket_alphas))
-assert(len(betas) == len(gasket_betas))
-for a in range(len(alphas)): assert(abs(alphas[a]-gasket_alphas[a])<1e-5)
-for b in range(len(betas)):  assert(abs(betas[b]-gasket_betas[b])<1e-5)
-
-
-
-#fig = plt.figure(num=None, figsize=(17, 12), dpi=80, facecolor='w', edgecolor='k')
-fig = plt.figure()
-scalarMap, colorBar = prepPlot(alphas)
-
-
-for a in range(0,len(alphas)):
-    njoy_sym = [sab_nonsym_negative_side[b+a*len(betas)] * \
-                      np.exp(-betas[b]*0.5) for b in range(len(betas))]
-    gasket_sym = [0.02550730568*gasket_sab[b+a*len(betas)] for b in range(len(betas))]
-    difference = [gasket_sym[i] - njoy_sym[i] for i in range(len(njoy_sym))] 
-    plt.plot(betas,njoy_sym,color=scalarMap.to_rgba(a),linewidth=3)
-    plt.plot(betas[:-1],gasket_sym[:-1],color=scalarMap.to_rgba(a),\
-             marker='o',markersize=3,linewidth=2,linestyle='dashed')
-    #plt.plot(betas[:-1],difference[:-1],color=scalarMap.to_rgba(a))
+def checkThatAlphasBetasMatch(gasket_alphas,gasket_betas,njoy_alphas,njoy_betas):
+    assert(len(njoy_alphas) == len(gasket_alphas))
+    assert(len(njoy_betas) == len(gasket_betas))
+    for a in range(len(njoy_alphas)): assert(abs(njoy_alphas[a]-gasket_alphas[a])<1e-5)
+    for b in range(len(njoy_betas)):  assert(abs(njoy_betas[b]-gasket_betas[b])<1e-5)
 
 
+def doThePlotting(alphas,betas,sab1,sab2):
+    for a in range(len(alphas)):
+        #njoy_sym = [njoy_sab[b+a*len(betas)] * \
+        #            np.exp(-betas[b]*0.5) for b in range(len(betas))]
+        #gasket_sym = [0.02550730568*gasket_sab[b+a*len(betas)] for b in range(len(betas))]
+        plt.plot(betas,[sab1[b+a*len(betas)] for b in range(len(betas))],color=scalarMap.to_rgba(a),linewidth=3)
+        plt.plot(betas,[sab1[b+a*len(betas)] for b in range(len(betas))],color=scalarMap.to_rgba(a),\
+                 marker='o',markersize=3,linewidth=2,linestyle='dashed')
+        #difference = [(gasket_sym[i] - njoy_sym[i])/(njoy_sym[i]) for i in range(len(njoy_sym))] 
+        #plt.plot(betas[:-1],difference[:-1],color=scalarMap.to_rgba(a))
 
-ax = plt.gca()
-plt.colorbar(colorBar).ax.set_ylabel('alpha values')
-#plt.yscale('log')
-plt.show()
-#plt.draw()
-#plt.pause(1) 
-#input("<Hit Enter To Close>")
-#plt.close(fig)
+
+
+def getValuesFromInput(name):
+    
+    name = name.split('.') if '.' in name else \
+           name.split('_') if '_' in name else name.split()
+
+    if name[0] == 'g':
+        fileName = 'gasket_output_'+name[1]
+        alphas = importMod(fileName).alphas; betas  = importMod(fileName).betas
+        temp   = importMod(fileName).temp;   sab    = importMod(fileName).sab
+        sab    = [temp*val for val in sab]
+        return temp,alphas,betas,sab
+
+    elif name[0] == 'n':
+        fileName = 'njoy_output_'+name[1]
+        alphas = importMod(fileName).alphas; betas  = importMod(fileName).betas
+        temp   = importMod(fileName).temp;   sab    = importMod(fileName).sab
+        sab = [sab[b+a*len(betas)]*np.exp(-betas[b]*0.5) for b in range(len(betas)) for a in range(len(alphas))]
+        return temp,alphas,betas,sab
+
+
+
+
+
+
+if __name__=='__main__':
+    from importlib import import_module as importMod
+    if len(sys.argv) == 3:
+        T1,alphas1,betas1,sab1 = getValuesFromInput(sys.argv[1])
+        T2,alphas2,betas2,sab2 = getValuesFromInput(sys.argv[2])
+        checkThatAlphasBetasMatch(alphas1,betas1,alphas2,betas2)
+        alphas, betas = alphas1[:], betas1[:]
+        fig = plt.figure(num=None,figsize=(17,12),dpi=80,facecolor='w',edgecolor='k') #fig = plt.figure()
+        scalarMap, colorBar = prepPlot(alphas)
+        doThePlotting(alphas,betas,sab1,sab2)
+        endPlot(colorBar)
+
+
+   
+
+    
 
 
--- gasket/python/scint.py
+++ gasket/python/scint.py
@@ -1,5 +1,6 @@
 import numpy as np
 from numpy import sin,cos,exp,sinh,cosh,sqrt
+import time
 
 def SCINT( t, GC, GS, EPS, T, A, B, F):
     S = [0.0]*len(EPS)
@@ -9,31 +10,26 @@ def SCINT( t, GC, GS, EPS, T, A, B, F):
     Q1 = cos(B*GS[0])*EX1*EX2-EX2;
     R1 = sin(B*GS[0])*EX1*EX2;
 
+    #start= time.time()
     for i in range(len(EPS)):
         AL = A-EPS[i];
-        if (AL == 0): 
-            print("AL = 0") 
-            continue
-        S[i] = 0.0
+        if (AL == 0): print("AL = 0"); continue
         sinSM = sin(t[0]*AL);
         cosSM = cos(t[0]*AL);
-        V0 = 0.0
-        invV0 = 1.0e5
-        invV  = 0.0
+        V0 = 1e-5
         for j in range(1,len(t)):
             sinS = sin(t[j]*AL);
             cosS = cos(t[j]*AL);
             V = (t[j]-t[j-1])*AL;
-            invV = 1.0/V
-            if abs(V*invV0-1.) > 5e-7:
+            if abs(V/V0-1.) > 5e-7:
                 if abs(V) <= 0.005:
-                  ST = (V*V)/6.-(V**4)/120.0
-                  CT = V*0.5-V**3/24.0
+                  ST = (V*V)*0.166666666-(V**4)*0.00833333333
+                  CT = V*0.5-V**3*0.0416666666
                 else:
                   sinT = sinS*cosSM-cosS*sinSM;
                   cosT = cosS*cosSM+sinS*sinSM;
-                  ST =  1.0-sinT *invV
-                  CT = (1.0-cosT)*invV
+                  ST =  1.0-sinT /V
+                  CT = (1.0-cosT)/V
             EX1 = exp(B*GC[j]);
             EX2 = exp(-A*T*t[j]*t[j]);
             Q2 = cos(B*GS[j])*EX1*EX2-EX2;
@@ -43,10 +39,11 @@ def SCINT( t, GC, GS, EPS, T, A, B, F):
             sinSM = sinS;
             cosSM = cosS;
             V0 = V;
-            invV0 = invV
             Q1 = Q2;
             R1 = R2;
         S[i] *= F/AL;
+    #end = time.time()
+    #print("-----",end-start)
     return S;
 
 
