9fe782787f15c1dd86331f7b51ddd08bc18545a0
Amelia Trainer <ameliajo@mit.edu>
2019-10-07 13:36:00 -0400

    added a piece for convergence

--- gasket/adaptiveTimeMesh/adaptive_F_H.py
+++ gasket/adaptiveTimeMesh/adaptive_F_H.py
@@ -1,10 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from numpy import sin,cos,exp
+from numpy import sin,cos,exp,sinh,cosh
+from waterDataContinuous import *
 
-
-def func(x):
-    return 5.0*x*exp(-x)+exp(-x)
 def get_CT(x):
     return (1.0-cos(x))/x if x > 0.005 else x*0.5 - x**3/24.0
 def get_ST(x):
@@ -13,16 +11,21 @@ def get_sin(x):
     return sin(x) if x > 0.005 else x - x**3*0.1666666
 def get_F0(x):
     return sin(x)/x**2 - cos(x)/x if x > 0.005 else x/3.0 - (x**3)/30.0
- 
 
-rho = [func(x) for x in np.linspace(0,10,50)]
-rho = [0.0,rho[0]*0.5] + rho + [0.0]
-betas = np.linspace(1e-6,3.0,len(rho))
-#plt.plot(betas,rho,'ro'); plt.show(); exit()
+energies = X[:]
+rho      = Q[:]
+betas = [x/0.0255 for x in energies]
+coth = [cosh(beta*0.5)/sinh(beta*0.5) for beta in betas]
 
-def getFs(t,rho,betas):
-    if t == 0.0: return 0.0
-    Fs = rho[0]*t*get_F0(betas[0]*t)
+norm = 1.0 / (Q[0]*betas[0]/3.0 + np.trapz(Q,betas))
+
+def getFt(t,rho,betas):
+    assert(t>0.0)
+
+    C = get_CT( betas[0]*t)
+    S = get_sin(betas[0]*t)
+    Ht = Q[0]/(betas[0])*(coth[0]*(S-C)+C/(betas[0]*0.5))
+    Ft = rho[0]*t*get_F0(betas[0]*t)
     sinNext = sin(betas[0]*t)
     cosNext = cos(betas[0]*t)
 
@@ -32,34 +35,73 @@ def getFs(t,rho,betas):
         if abs(betas[j]/betas[j-1] - 1.0) > 5e-7:
             theta = (betas[j]-betas[j-1])*t;
             ST, CT = get_ST(theta), get_CT(theta)
-        Fs += rho[j  ]/betas[j  ] * (CT*sinNext - ST*cosNext ) + \
+        Ht += Q[j  ]/betas[j  ] * (ST*sinNext + CT*cosNext ) * coth[j  ] - \
+              Q[j-1]/betas[j-1] * (ST*sinLast - CT*cosLast ) * coth[j-1]
+        Ft += rho[j  ]/betas[j  ] * (CT*sinNext - ST*cosNext ) + \
               rho[j-1]/betas[j-1] * (CT*sinLast + ST*cosLast )
-    return Fs/t
+    return Ft*norm/t,Ht*norm/t
 
 def getF(rho,betas,spacing,max_t_val,tolerance):
-    xL = 0.0;        yL = getFs(xL,rho,betas); final_X = [xL]
-    xR = xL+spacing; yR = getFs(xR,rho,betas); final_Y = [yL]
+    xL = 0.0; final_X = [xL]
+
+    # Figure out what t = 0 contributions will be
+    F0 = 0.0
+    H0 = Q[0]/betas[0] + 0.5*Q[0]*coth[0] + np.trapz([Q[i]*coth[i]/betas[i] for i in range(len(betas))],betas)
+    yL = (F0,H0)
+
+    xR = xL+1e-7; yR = getFt(xR,rho,betas)
+    final_F = [yL[0]]
+    final_H = [yL[1]]
+    max_F = 0.0
+    max_H = 0.0
     while xR < max_t_val:
         xM = (xL+xR)*0.5
-        yM = getFs(xM,rho,betas)
-        guess = (yL+yR)*0.5
-        if abs(guess-yM) < tolerance:
+        yM = getFt(xM,rho,betas)
+        guessF = (yL[0]+yR[0])*0.5
+        guessH = (yL[1]+yR[1])*0.5
+        error1 = abs(guessF-yM[0]) if yM[0] < 1e-5 else abs((guessF-yM[0])/yM[0])
+        error2 = abs(guessH-yM[1]) if yM[1] < 1e-5 else abs((guessH-yM[1])/yM[1]) 
+        if error1 < tolerance and (xL == 0.0 or error2 < tolerance):
             final_X.append(xR)
-            final_Y.append(yR)
+            final_F.append(yR[0])
+            final_H.append(yR[1])
+            if abs(yR[0]) > max_F: max_F = abs(yR[0])
+            if abs(yR[1]) > max_H: max_H = abs(yR[1])
             xL = xR; xR = xL + spacing
-            yL = yR; yR = getFs(xR,rho,betas)
+            yL = yR; yR = getFt(xR,rho,betas)
+            if len(final_X) > 20:
+                if max([abs(x) for x in final_F[-20:]]) < 1e-3*max_F and \
+                   max([abs(x) for x in final_H[-20:]]) < 1e-3*max_H:
+                    return final_X,final_F,final_H
         else:
             xR = xM; yR = yM
-    return final_X,final_Y
+    return final_X,final_F,final_H
 
-spacing = 1.0
+spacing = 2.0
 max_t_val = 100.0
 tolerance = 1e-3
-final_X, final_Y = getF(rho,betas,spacing,max_t_val,tolerance)
+final_X, final_F, final_H = getF(rho,betas,spacing,max_t_val,tolerance)
 
-plt.plot(final_X,final_Y,'ro')
-plt.plot(final_X,[0.0]*len(final_X),'go')
+inv_area = 1.0/np.trapz(final_F,final_X); final_F = [inv_area * F for F in final_F]
+inv_area = 1.0/np.trapz(final_H,final_X); final_H = [inv_area * H for H in final_H]
+
+
+plt.plot(final_X,final_F)
+plt.plot(final_X,final_H)
+#plt.plot(final_X,contrib_F)
+#plt.plot(final_X,contrib_H)
+
+#plt.plot(final_X,[0.0]*len(final_X),'go')
+
+#time = np.linspace(0,max_t_val,200)
+#uniform_F = [getFt(t,rho,betas) for t in time]
+#plt.plot(time,uniform_F)
+
+
 plt.show()
+
+
+
 
 
 
