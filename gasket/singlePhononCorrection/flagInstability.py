6a1b82266bc51668dbc5efceedc40ef24301d0ed
Amelia Trainer <ameliajo@mit.edu>
2019-11-27 15:28:22 -0500

    now have a good plot via flaginstability that shows how the single phonon can takeover

--- gasket/singlePhononCorrection/flagInstability.py
+++ gasket/singlePhononCorrection/flagInstability.py
@@ -45,14 +45,13 @@ def areWeUnstable(a,b,betas,sab,alpha_H_exp,cos_alpha_F,sin_alpha_F,time,alpha_e
 def simpleGASKET(rhoBetas,rho,time,alphas,betas,flagUnstable = False):
     invArea = 1.0/np.trapz(rho,rhoBetas)
     rho     = [invArea * x for x in rho]
+    print('getting F and H...')
     F, H    = get_F_H(rhoBetas,rho,time)
-    #plt.plot(time,F); plt.plot(time,H)
-    #plt.show(); exit()
+    plt.plot(time,F); plt.plot(time,H)
+    plt.show(); exit()
 
     sab       = [0.0]*len(betas)*len(alphas)
     alpha_exp = [exp(-alpha*H[0])/pi for alpha in alphas] # H[0] = debye waller 
-    #beta_exp  = [exp(beta*0.5)       for beta  in betas ] # This is to turn 
-                                         # the scattering law to be symmetric
 
     unstable = False
     for a,alpha in enumerate(alphas):
@@ -67,18 +66,20 @@ def simpleGASKET(rhoBetas,rho,time,alphas,betas,flagUnstable = False):
             sab[b+a*len(betas)] = np.trapz(integrand,time) * alpha_exp[a] 
 
             if b+a*len(betas) < 2: continue
-            
 
             if not unstable:
-                unstable = areWeUnstable(a,b,betas,sab,alpha_H_exp,cos_alpha_F,sin_alpha_F,time,alpha_exp)
+                unstable = areWeUnstable(a,b,betas,sab,alpha_H_exp,cos_alpha_F,\
+                                         sin_alpha_F,time,alpha_exp)
                 if unstable == True:
                     for i in range(-5,0):
-                        sab_approx = getSAB_T1_approx(rhoBetas,rho,[alphas[a]],[betas[b+i]])[0]*exp(-betas[b])
+                        sab_approx = getSAB_T1_approx(rhoBetas,rho,[alphas[a]],\
+                                     [betas[b+i]])[0]*exp(-betas[b])
                         plt.plot(betas[b+i],sab_approx,'yo')
  
             if unstable:
                 plt.plot(beta,sab[b+a*len(betas)],'ro')
-                sab_approx = getSAB_T1_approx(rhoBetas,rho,[alphas[a]],[betas[b]])[0]*exp(-betas[b])
+                sab_approx = getSAB_T1_approx(rhoBetas,rho,[alphas[a]],\
+                             [betas[b]])[0]*exp(-betas[b])
                 plt.plot(beta,sab_approx,'yo')
             else:
                 plt.plot(beta,sab[b+a*len(betas)],'bo')
@@ -96,28 +97,22 @@ if __name__=="__main__":
     sys.path.append("../../phononExpansion"); from getSAB_phononExpansion import *
     from colors import misccolors
 
-
-
-    kbT = 8.61733e-5 * 293.0
-    invT = 1.0/kbT
-    #rhoBetas = [rhoX_val*invT for rhoX_val in X]
-
     alphas = [0.001]
     betas  = list(np.linspace(0,5,51))
+    time = np.linspace(0,100,1e3)
 
-    NT1 = 1e4; time1 = np.linspace(0,120,NT1)
-
-    sab,H,F = simpleGASKET(uniform_x[1:],uniform_y[1:],time1,alphas,betas)
+    sab,H,F = simpleGASKET(uniform_x[1:],uniform_y[1:],time,alphas,betas)
     print("Just finished gasket")
 
     for a in range(len(alphas)):
         plt.plot(betas,[sab[b+a*len(betas)] for b in range(len(betas))],\
                  color=misccolors[a],linewidth=1.5,alpha=0.8)
 
-
-        sab_nonsym_neg_side = getSAB_phononExpansion(5,uniform_x[1]-uniform_x[0],uniform_y,[alphas[a]],betas)
+        sab_nonsym_neg_side = getSAB_phononExpansion(5,uniform_x[1]-uniform_x[0],\
+                              uniform_y,[alphas[a]],betas)
         print("Just finished phonon distribution")
-        sab_nonsym_pos_side = [sab_nonsym_neg_side[b]*exp(-betas[b]) for b in range(len(betas))]
+        sab_nonsym_pos_side = [sab_nonsym_neg_side[b]*exp(-betas[b]) \
+                               for b in range(len(betas))]
         plt.plot(betas,sab_nonsym_pos_side,'go')
 
     plt.yscale('log')
