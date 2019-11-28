ef55724a44b2f388ffc6e47dd6c02306f9ce57b6
Amelia Trainer <ameliajo@mit.edu>
2019-11-27 21:28:47 -0500

    cleaning up the gasket correction code

--- gasket/singlePhononCorrection/flagInstability.py
+++ gasket/singlePhononCorrection/flagInstability.py
@@ -12,80 +12,57 @@ sys.path.append('../../phononExpansion/help')
 from getSAB_T1_approx import *
 from get_F_H import *
 
-def areWeUnstable(a,b,betas,sab,alpha_H_exp,cos_alpha_F,sin_alpha_F,time,alpha_exp):
-    F1_x = betas[b-2]; F1_y = sab[b+a*len(betas)-2]
-    F2_x = betas[b-1]; F2_y = sab[b+a*len(betas)-1]
-    F3_x = betas[b  ]; F3_y = sab[b+a*len(betas)  ]
 
-    if F2_y > F1_y and F3_y < F2_y: # If we're not monotonically increasing or decreasing
-        N = 20
-        sab_i_vec  = []
-        num_osc = 0
-        for i in range(N):
-            beta_i = i*(F3_x-F2_x)/N + F2_x
-            integrand = [alpha_H_exp[t] * cos_alpha_F[t] * cos(beta_i*time[t]) \
-                       - alpha_H_exp[t] * sin_alpha_F[t] * sin(beta_i*time[t]) \
-                       - cos(beta_i*time[t]) for t in range(len(time))]
+def largeDifference(sab1, sab2):
+    return abs(sab2-sab1) > (abs(0.5*(sab2+sab1)))
 
-            sab_at_beta_i = np.trapz(integrand,time) * alpha_exp[a] #* exp(beta_i*0.5)
-            sab_i_vec.append(sab_at_beta_i)
-            if len(sab_i_vec) > 2:
-                if sab_i_vec[-2] - sab_i_vec[-1] > 0.6 * (F3_y-F2_y) and \
-                   sab_i_vec[-2] > sab_i_vec[-3]:
-                    num_osc += 1
-            if sab_at_beta_i < 0:
-                return True
-        if num_osc > 2:
-            return True
-    return False
+def largeDifferences(sabChunk):
+    for i in range(len(sabChunk)-1):
+        if not largeDifference(sabChunk[i],sabChunk[i+1]):
+            return False
+    return True
+
+def slopesDiffer(sabChunk):
+    signs = [None]*(len(sabChunk)-1)
+    for i in range(1,len(sabChunk)-1):
+        if np.sign(sabChunk[i  ]-sabChunk[i-1]) == \
+           np.sign(sabChunk[i+1]-sabChunk[i  ]):
+            return False
+    return True
+
 
 
 
 
 def simpleGASKET(rhoBetas,rho,time,alphas,betas,flagUnstable = False):
-    invArea = 1.0/np.trapz(rho,rhoBetas)
-    rho     = [invArea * x for x in rho]
     print('getting F and H...')
+    invArea = 1.0/np.trapz(rho,rhoBetas); rho = [invArea * x for x in rho]
     F, H    = get_F_H(rhoBetas,rho,time)
-    plt.plot(time,F); plt.plot(time,H)
-    plt.show(); exit()
 
     sab       = [0.0]*len(betas)*len(alphas)
     alpha_exp = [exp(-alpha*H[0])/pi for alpha in alphas] # H[0] = debye waller 
 
-    unstable = False
     for a,alpha in enumerate(alphas):
         alpha_H_exp = [exp(alpha*H[t]) for t in range(len(time))]
         sin_alpha_F = [sin(alpha*F[t]) for t in range(len(time))]
         cos_alpha_F = [cos(alpha*F[t]) for t in range(len(time))]
-        numNeg = 0
+
+        oscBegin = None
+        treatOsc   = None
+
         for b,beta in enumerate(betas):
+            i = b+a*len(betas)
             integrand = [alpha_H_exp[t] * cos_alpha_F[t] * cos(beta*time[t]) \
                        - alpha_H_exp[t] * sin_alpha_F[t] * sin(beta*time[t]) \
                        - cos(beta*time[t]) for t in range(len(time))]
-            sab[b+a*len(betas)] = np.trapz(integrand,time) * alpha_exp[a] 
+            sab[i] = np.trapz(integrand,time) * alpha_exp[a] 
 
-            if b+a*len(betas) < 2: continue
+            if i > 5 and oscBegin == None: 
+                if largeDifferences(sab[i-3:i+1]) and slopesDiffer(sab[i-3:i+1]):
+                    oscBegin = betas[b]
+                    treatOsc   = betas[b-5] 
 
-            if not unstable:
-                unstable = areWeUnstable(a,b,betas,sab,alpha_H_exp,cos_alpha_F,\
-                                         sin_alpha_F,time,alpha_exp)
-                if unstable == True:
-                    for i in range(-5,0):
-                        sab_approx = getSAB_T1_approx(rhoBetas,rho,[alphas[a]],\
-                                     [betas[b+i]])[0]*exp(-betas[b])
-                        plt.plot(betas[b+i],sab_approx,'yo')
- 
-            if unstable:
-                plt.plot(beta,sab[b+a*len(betas)],'ro')
-                sab_approx = getSAB_T1_approx(rhoBetas,rho,[alphas[a]],\
-                             [betas[b]])[0]*exp(-betas[b])
-                plt.plot(beta,sab_approx,'yo')
-            else:
-                plt.plot(beta,sab[b+a*len(betas)],'bo')
-
-
-    return sab,H,F
+    return sab,oscBegin,treatOsc
 
 
 
@@ -98,24 +75,30 @@ if __name__=="__main__":
     from colors import misccolors
 
     alphas = [0.001]
-    betas  = list(np.linspace(0,5,51))
-    time = np.linspace(0,100,1e3)
+    alphas = [0.001]
+    betas  = list(np.linspace(0,13,81))
+    nbeta = len(betas)
+    time = np.linspace(0,96.2,8e2)
+    delta = uniform_x[1]-uniform_x[0]
 
-    sab,H,F = simpleGASKET(uniform_x[1:],uniform_y[1:],time,alphas,betas)
+    sab,oscBegin,treatOsc = simpleGASKET(uniform_x[1:],uniform_y[1:],time,alphas,betas)
     print("Just finished gasket")
 
     for a in range(len(alphas)):
-        plt.plot(betas,[sab[b+a*len(betas)] for b in range(len(betas))],\
-                 color=misccolors[a],linewidth=1.5,alpha=0.8)
+        plt.plot(betas,[sab[b+a*nbeta] for b in range(nbeta)],'ro',label='GASKET',linestyle='solid')
+        if oscBegin != None:
+            plt.plot([oscBegin,oscBegin],[1e-8,1e-2],'y',label='First detectable osc.')
+            plt.plot([treatOsc,treatOsc],[1e-8,1e-2],'g',label='Start treting osc.')
 
-        sab_nonsym_neg_side = getSAB_phononExpansion(5,uniform_x[1]-uniform_x[0],\
-                              uniform_y,[alphas[a]],betas)
+    nphon = 10
+    for a in range(len(alphas)):
+        sab_neg_side = getSAB_phononExpansion(nphon,delta,uniform_y,[alphas[a]],betas)
+        sab_pos_side = [sab_neg_side[b]*exp(-betas[b]) for b in range(nbeta)]
         print("Just finished phonon distribution")
-        sab_nonsym_pos_side = [sab_nonsym_neg_side[b]*exp(-betas[b]) \
-                               for b in range(len(betas))]
-        plt.plot(betas,sab_nonsym_pos_side,'go')
+        plt.plot(betas,sab_pos_side,'bo',label='Phonon Expansion')
 
     plt.yscale('log')
+    plt.legend(loc='best')
     plt.show()
 
 
